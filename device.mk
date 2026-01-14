1bf224a5194f7bed69bc4b8ac47d0e7ba1ef19ee
John Galt <johngaltfirstrun@gmail.com>
2026-01-14 09:50:56 -0500

    [squash] dodge: set target min/max supported frame rate of 1 / 120
    Change-Id: I1fcf176af1a009e8d6b81dd48f047080e396f030
    Signed-off-by: klozz <carlosj@klozz.dev>

--- device.mk
+++ device.mk
@@ -23,6 +23,9 @@ TARGET_SCREEN_HEIGHT := 3168
 TARGET_SCREEN_WIDTH := 1440
 
 # Display
+$(call soong_config_set,surfaceflinger,frame_rate_category_high,120)
+$(call soong_config_set,surfaceflinger,frame_rate_category_min,1)
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display/displayconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946756802996883.xml
 
