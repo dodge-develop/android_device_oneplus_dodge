c72ec4a16ddbe3ce8fa475acd1b37103ec368748
Hecheng Yu <kde-yyds@qq.com>
2026-03-27 17:00:51 +0800

    dodge: Add support for OPlus Camera
    Squashed:
        Include vendor/oplus/camera/
        Add BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true for prop ro.oplus.*
        Revert CameraHWConfiguration.config changes to re-enable fd support
        Use stock old libui.so and libutils.so to fix crash at cleanup

--- device.mk
+++ device.mk
@@ -88,3 +88,6 @@ $(call inherit-product, device/oneplus/sm8750-common/common.mk)
 
 # Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/oneplus/dodge/dodge-vendor.mk)
+
+# Camera
+$(call inherit-product-if-exists, vendor/oplus/camera/opluscamera.mk)
