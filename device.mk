ca558abdb7a8b124be06feefb834b09adb17f0d3
John Galt <johngaltfirstrun@gmail.com>
2026-05-07 16:16:37 -0400

    dodge: enable oplus anchor list
    Change-Id: Ifa129b67787627355b4d5fe4a1cec9b0a0ee1f30

--- device.mk
+++ device.mk
@@ -25,6 +25,7 @@ TARGET_SCREEN_WIDTH := 1440
 # Display
 $(call soong_config_set,surfaceflinger,frame_rate_category_high,120)
 $(call soong_config_set,surfaceflinger,frame_rate_category_min,1)
+$(call soong_config_set,surfaceflinger,arr_use_oplus_ltpo_rates,true)
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display/displayconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946756802996883.xml
