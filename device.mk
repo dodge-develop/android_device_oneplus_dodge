3d1cab52b8c6d63168178a835f04a3c3c62dfaeb
likw <1912283462@qq.com>
2026-05-12 17:21:09 +0800

    dodge: High touch polling rate
    Change-Id: I2cb867bd9d96eff6b29492a2055721ad90f1900f

--- device.mk
+++ device.mk
@@ -79,7 +79,7 @@ PRODUCT_COPY_FILES += \
 
 # Touch features
 $(call soong_config_set_bool,OPLUS_LINEAGE_TOUCH_HAL,ENABLE_GM,true)
-$(call soong_config_set_bool,OPLUS_LINEAGE_TOUCH_HAL,ENABLE_HTPR,false)
+$(call soong_config_set_bool,OPLUS_LINEAGE_TOUCH_HAL,ENABLE_HTPR,true)
 
 # Vibrator
 PRODUCT_COPY_FILES += \
