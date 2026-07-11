5f9427835ac691826739ff7d0412d790f5500cb0
likw <1912283462@qq.com>
2026-07-11 20:27:06 +0800

    dodge: Bump min refresh rate to 10hz https://github.com/yaap/device_oneplus_waffle/commit/29b09099defe175e7d3dd9e4440ed0ba1cd26387
    Change-Id: I5b79201d5952727cf9354ecca02bd2249d3b8b73

--- device.mk
+++ device.mk
@@ -24,7 +24,7 @@ TARGET_SCREEN_WIDTH := 1440
 
 # Display
 $(call soong_config_set,surfaceflinger,frame_rate_category_high,120)
-$(call soong_config_set,surfaceflinger,frame_rate_category_min,1)
+$(call soong_config_set,surfaceflinger,frame_rate_category_min,10)
 $(call soong_config_set,surfaceflinger,arr_use_oplus_ltpo_rates,true)
 $(call soong_config_set,qtidisplay,bitclk_rate_permissive,true)
 
